31c2ae063c24d92f07b57b32be17bb82ce8e6b49
antoine <antoine.robert57@icloud.com>
2019-08-28 22:07:03 +0200

    fix env.prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://mep.bober.ovh/api',
+  apiUrl: 'https://mep.bober.ovh/api',
   paths: {
     meps: '/meps',
     templates: '/templates',
